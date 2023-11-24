e392bc3911f2ce0faeccd33a21091b20f378ecea
Sasha Rocha <sasharocha@desktop.local>
2023-11-24 13:23:03 -0300

    refactorizacion de archivos

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mainRoutes = require('./src/routes/mainRoutes');
-const shopRoutes = require('./src/routes/shopRoutes');
-const adminRoutes = require('./src/routes/adminRoutes');
-const authRoutes = require('./src/routes/authRoutes');
+const mainRoutes = require("./src/routes/mainRoutes");
+const shopRoutes = require("./src/routes/shopRoutes");
+const adminRoutes = require("./src/routes/adminRoutes");
+const authRoutes = require("./src/routes/authRoutes");
 
+app.use(express.static("public"));
 
-
-app.use(express.static('public'));
-
-app.use('/', mainRoutes);
-app.use('/shop', shopRoutes);
-app.use('/admin', adminRoutes);
-app.use('/auth', authRoutes);
-
-
+app.use("/", mainRoutes);
+app.use("/shop", shopRoutes);
+app.use("/admin", adminRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(3000, () => console.log("servidor  http://localhost:3000"));
