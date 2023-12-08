ae7832604bc132b6271334578eec6a56aefa1b9f
Daira <barretodairatamara@gmail.com>
2023-12-08 01:24:45 -0300

    Conectando base de datos a shop, index e item [V2 - Exitosa]

--- app.js
+++ app.js
@@ -4,18 +4,12 @@ import methodOverride from 'method-override';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { createRequire } from 'module'; 
-const require = createRequire(import.meta.url);
-const productos = require('./productos.json');
-
 
 /* Importación de Rutas */
 import mainRoutes from './src/routes/mainRoutes.js';
 import shopRoutes from './src/routes/shopRoutes.js';
 import adminRoutes from './src/routes/adminRoutes.js';
 import authRoutes from './src/routes/authRoutes.js';
-
-console.log(productos);
 
 dotenv.config()
 
@@ -26,19 +20,17 @@ const __dirname = dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+/* Carga de archivos estáticos */
+app.use(express.static(path.join(__dirname, 'public')));
+
 /* Motor Plantillas EJS */
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static('public'));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(methodOverride('metodo'));
-app.use(methodOverride('put'));
-app.use(methodOverride('delete'));
-
 
 /* Rutas */
 app.use('/', mainRoutes);  
@@ -49,17 +41,13 @@ app.use('/auth', authRoutes);
 
 /* Ícono */
 app.use((req, res, next) => {
-  res.locals.iconPath = '/Assets/Img/branding/isotype.svg';
+  res.locals.iconPath = path.join(__dirname, '../Assets/Img/branding/isotype.svg');
   next();
 });
 
 /* Redirección al Home al iniciar el server*/
 app.get('/', (req, res) => {
-    res.redirect('/home');
-});
-
-app.get('/home', (req, res) => {
-  res.render('index', { title: 'Mi Página EJS' });
+  res.redirect('/home');
 });
 
 app.listen(PORT, () => {
