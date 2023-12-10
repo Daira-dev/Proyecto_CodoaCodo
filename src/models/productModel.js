4652eee715b698c9019de6df6bb78c6a5821171d
Daira <barretodairatamara@gmail.com>
2023-12-10 14:18:15 -0300

    Arreglando DB incluyendo tablas [admin y edit]

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -6,17 +6,24 @@ const productModel = {
       const [rows, fields] = await pool.query('SELECT * FROM products');
       return rows;
     } catch (error) {
-      console.error('Error fetching products', error);
+      console.error('Error encontrando el producto', error);
       throw error;
     }
   },
 
   getProductById: async (productId) => {
     try {
-      const [rows, fields] = await pool.query('SELECT * FROM products WHERE product_id = ?', [productId]);
+      const [rows, fields] = await pool.query(`
+        SELECT products.*, category.category_name, licence.licence_name
+        FROM products
+        JOIN category ON products.category_id = category.category_id
+        JOIN licence ON products.licence_id = licence.licence_id
+        WHERE products.product_id = ?
+      `, [productId]);
+
       return rows[0];
     } catch (error) {
-      console.error('Error fetching product by ID', error);
+      console.error('Error encontrando el producto by ID', error);
       throw error;
     }
   },
